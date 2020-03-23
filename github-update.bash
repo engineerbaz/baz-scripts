cf8f9f33958112614cbc7276ed057b23e4d180a5
Bashir Ahmed Zeeshan <56934817+engineerbaz@users.noreply.github.com>
2020-03-23 07:55:15 +0500

    Update github-update.bash

--- github-update.bash
+++ github-update.bash
@@ -1,14 +1,12 @@
 # Script for upadting file to GtHub by taking filename as input.
 
-```bash
+```
 RED='\033[0;31m'
 NC='\033[0m' 
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
-
 echo -e "\n****** Welcome to ${RED}Git${NC} Script by ${GREEN}BAZ${NC} **$
 echo "This script will upadte file to GtHub by taking filename input!"
-
 ls
 git status
 echo -e "\n ${BLUE}Please enter file name you want to upload to remote re$
@@ -16,8 +14,6 @@ read fname
 git add $fname
 git commit -m "update on file $fname"
 git push origin master
-
 echo "     "
 echo -e "--------- ${BLUE}Completed update on Git ------"
-
 ```
